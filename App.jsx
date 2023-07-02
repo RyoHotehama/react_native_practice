cb45c1c44f2f4bb2b37a60c7382fd870ddb4f232
hotehama2022 <hotehama-r@cin-group.com>
2023-07-02 11:29:07 +0900

    useStateの利用 FlatListの使い方

--- App.jsx
+++ App.jsx
@@ -1,11 +1,33 @@
-import { Platform, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Platform, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 50 : StatusBar.currentHeight;
 
 export default function App() {
+  const [items] = useState(
+    {
+      todo: [
+        {index: 1, title: '原稿を書く', done: false},
+        {index: 2, title: '犬の散歩をする', done: false},
+      ],
+      currentIndex: 2
+    }
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.filter}>
+        <Text>Filterがここに配置されます</Text>
+      </View>
+      <ScrollView style={styles.todolist}>
+        <FlatList
+          data={items.todo}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+          keyExtractor={(item) => 'todo_' + item.index}
+        />
+      </ScrollView>
+      <View style={styles.input}>
+        <Text>テキスト入力がここに配置されます</Text>
+      </View>
     </View>
   );
 }
@@ -15,5 +37,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: STATUSBAR_HEIGHT,
+  },
+  filter: {
+    height: 30,
+  },
+  todolist: {
+    flex: 1,
+  },
+  input: {
+    height: 30,
   },
 });
